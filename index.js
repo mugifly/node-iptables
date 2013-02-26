cd47c73cea75792a7a1f117603ea1366a3c99265
Ken Perkins <ken@ksperkins.com>
2013-02-26 11:35:17 -0800

    Hookup underscore parsing

--- index.js
+++ index.js
@@ -44,32 +44,7 @@ exports.list = function(chain, callback) {
             return;
         }
 
-        lazy(output.stdout)
-            .lines
-            .map(String)
-            .skip(2)
-            .map(function(line) {
-                // packets, bytes, target, pro, opt, in, out, src, dst, opts
-                var fields = line.trim().split(/\s+/, 9);
-                return {
-                    parsed: {
-                        packets: fields[0],
-                        bytes: fields[1],
-                        target: fields[2],
-                        protocol: fields[3],
-                        opt: fields[4],
-                        in: fields[5],
-                        out: fields[6],
-                        src: fields[7],
-                        dst: fields[8]
-                    },
-                    raw: line.trim()
-                };
-
-            })
-            .join(function(rules) {
-                callback(err, rules);
-            });
+        callback(err, _.lines(output.stdout));
     });
 };
 
