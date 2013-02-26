9042b832075451a984042be259668ac19617b5de
Ken Perkins <ken@ksperkins.com>
2013-02-26 11:12:29 -0800

    Adding support for callbacks with iptables

--- index.js
+++ index.js
@@ -1,35 +1,35 @@
-var spawn = require('child_process').spawn;
+var exec = require('child_process').exec;
 var lazy = require('lazy');
 
-exports.allow = function (rule) {
+exports.allow = function (rule, callback) {
     rule.target = 'ACCEPT';
     if (!rule.action) rule.action = '-A';
-    newRule(rule);
+    newRule(rule, callback);
 };
 
-exports.drop = function (rule) {
+exports.drop = function (rule, callback) {
     rule.target = 'DROP';
     if (!rule.action) rule.action = '-A';
-    newRule(rule);
+    newRule(rule, callback);
 };
 
-exports.reject = function (rule) {
+exports.reject = function (rule, callback) {
     rule.target = 'REJECT';
     if (!rule.action) rule.action = '-A';
-    newRule(rule);
+    newRule(rule, callback);
 };
 
-exports.policy = function(chain, target) {
+exports.policy = function(chain, target, callback) {
     var rule = {
         chain: chain,
         action: '-P',
         target: target
     };
 
-    newRule(rule);
+    newRule(rule, callback);
 };
 
-exports.list = function(chain, cb) {
+exports.list = function(chain, callback) {
     var rule = {
         list : true,
         chain : chain,
@@ -37,50 +37,58 @@ exports.list = function(chain, cb) {
         sudo : true
     };
 
-    lazy(iptables(rule).stdout)
-        .lines
-        .map(String)
-        .skip(2)
-        .map(function (line) {
-            // packets, bytes, target, pro, opt, in, out, src, dst, opts
-            var fields = line.trim().split(/\s+/, 9);
-            return {
-                parsed : {
-                    packets : fields[0],
-                    bytes : fields[1],
-                    target : fields[2],
-                    protocol : fields[3],
-                    opt : fields[4],
-                    in : fields[5],
-                    out : fields[6],
-                    src : fields[7],
-                    dst : fields[8]
-                },
-                raw : line.trim()
-            };
-        })
-        .join(function (rules) {
-            cb(rules);
-        });
+    iptables(rule, function(err, output) {
+        if (err) {
+            callback(err);
+            return;
+        }
+
+        lazy(output.stdout)
+            .lines
+            .map(String)
+            .skip(2)
+            .map(function(line) {
+                // packets, bytes, target, pro, opt, in, out, src, dst, opts
+                var fields = line.trim().split(/\s+/, 9);
+                return {
+                    parsed: {
+                        packets: fields[0],
+                        bytes: fields[1],
+                        target: fields[2],
+                        protocol: fields[3],
+                        opt: fields[4],
+                        in: fields[5],
+                        out: fields[6],
+                        src: fields[7],
+                        dst: fields[8]
+                    },
+                    raw: line.trim()
+                };
+
+            })
+            .join(function(rules) {
+                callback(err, rules);
+            });
+    });
 };
 
 exports.newRule = newRule;
 exports.deleteRule = deleteRule;
 
-function iptables (rule) {
+function iptables(rule, callback) {
     var args = iptablesArgs(rule);
 
-    var cmd = 'iptables';
+    var cmd = ['iptables'].concat(args);
     if (rule.sudo) {
-        cmd = 'sudo';
-        args = ['iptables'].concat(args);
+        cmd = ['sudo'].concat(cmd);
     }
 
-    var proc = spawn(cmd, args);
-    proc.stderr.on('data', function (buf) {
-        console.error(buf.toString());
+    exec(cmd.join(' '), function(err, stdout, stderr) {
+        callback(err, {
+            stdout: stdout.toString(),
+            stderr: stderr.toString()
+        });
     });
-    return proc;
 }
 
 function iptablesArgs (rule) {
@@ -109,12 +117,12 @@ function iptablesArgs (rule) {
     return args;
 }
 
-function newRule (rule) {
-    iptables(rule);
+function newRule (rule, callback) {
+    iptables(rule, callback);
 }
 
-function deleteRule (rule) {
+function deleteRule (rule, callback) {
     rule.action = '-D';
-    iptables(rule);
+    iptables(rule, callback);
 }
 
